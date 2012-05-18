12e163caa8100bfbb6c8e5e2c34bb6ec52d24305
Hidehito Nozawa aka Suin <suinyeze@gmail.com>
2012-05-18 13:32:00 +0900

    add ScrollToAgreeTOS

---
+++ README.md
@@ -0,0 +1,10 @@
+# XOOPS Cube プリロード集
+
+XOOPS Cube で使えるプリロードを集めてみました。
+
+## 目次
+
+* [JquerySlideShow.class.php](https://raw.github.com/suin/xoopscube-preloads/master/JquerySlideShow/JquerySlideShow.class.php) : こういう( http://wex.im/ )スライドショーをXOOPS Cubeで使えるようにするプリロード* 
+* [ModuleAruno.class.php](https://raw.github.com/suin/xoopscube-preloads/master/ModuleAruno/ModuleAruno.class.php) : モジュールあるの？という疑問に答えるプリロード
+* [SampleFetchBlockContents.class.php](https://raw.github.com/suin/xoopscube-preloads/master/SampleFetchBlockContents/SampleFetchBlockContents.class.php) : プリロードで特定のブロックの実行結果をSmartyに割り当てるサンプル
+* [SampleGetBlockContent.class.php](https://raw.github.com/suin/xoopscube-preloads/master/SampleGetBlockContent/SampleGetBlockContent.class.php) :* 
---
+++ ScrollToAgreeTOS/ScrollToAgreeTOS.class.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * ユーザ登録時、利用規約を最後までスクロールしないと登録できなくするプリロード
+ */
+class ScrollToAgreeTOS extends XCube_ActionFilter
+{
+	/**
+	 * Pre-filter
+	 */
+	public function preFilter()
+	{
+		$this->mRoot->mDelegateManager->add('Legacypage.Register.Access', array($this, 'registerScrollToAgreeScript'), XCUBE_DELEGATE_PRIORITY_FIRST);
+	}
+
+	/**
+	 * スクロール同意スクリプトを登録する
+	 */
+	public function registerScrollToAgreeScript()
+	{
+		$script = $this->_getJQueryScript();
+		$this->_addHeaderScript($script);
+	}
+
+	/**
+	 * Return Legacy_HeaderScript object
+	 * @return Legacy_HeaderScript
+	 */
+	protected function _getHeaderScript()
+	{
+		return $this->mRoot->mContext->getAttribute('headerScript');
+	}
+
+	/**
+	 * Add jQuery script
+	 * @param string $jQuery
+	 */
+	protected function _addHeaderScript($jQuery)
+	{
+		$headerScript = $this->_getHeaderScript();
+		$headerScript->addScript($jQuery, false);
+	}
+
+	/**
+	 * Return jQuery script
+	 * @return string
+	 */
+	protected function _getJQueryScript()
+	{
+		ob_start();
+		?>
+		//<script type="text/javascript">
+		jQuery(function($){
+			var TEXTAREA = 'textarea[name=disclaimer]';
+			var CHECK_BOX = 'input[type=checkbox][name=agree]';
+			var SUBMIT_BUTTON = 'input[type=submit]';
+
+			if ( $(TEXTAREA).length == 0 ) {
+				return;
+			}
+
+			if ( $(CHECK_BOX).length == 0 ) {
+				return;
+			}
+
+			var submitButton = $(TEXTAREA).parents('form').find(SUBMIT_BUTTON);
+
+			$(CHECK_BOX).parent().hide();
+			$('<div />').text('最後までスクロールしてお読みいただくと送信ボタンが押せるようになります。').insertAfter(TEXTAREA);
+
+			if ( $(CHECK_BOX).is(':checked') == false ) {
+				$(submitButton).attr('disabled', 'disabled');
+			}
+
+			$(TEXTAREA).bind('scroll', function(){
+				if ( $(this).prop('scrollTop') + $(this).prop('offsetHeight') > $(this).prop('scrollHeight') ) {
+					$(CHECK_BOX).attr('checked', 'checked');
+					$(submitButton).removeAttr('disabled');
+				}
+			});
+		});
+		//</script>
+		<?php
+		return ob_get_clean();
+	}
+}
